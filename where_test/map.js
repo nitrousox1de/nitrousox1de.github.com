175d5a4e8a51a879413107cffca51b5a6a595947
Andrew <andrew.li@tufts.edu>
2013-02-27 20:06:13 -0500

    update to a3

--- where_test/map.js
+++ where_test/map.js
@@ -140,3 +140,18 @@ function callback(results, status)
         }
     }
 }
+
+function createMarker(place)
+            {
+                var placeLoc = place.geometry.location;
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.close();
+                    infowindow.setContent(place.name);
+                    infowindow.open(map, this);
+                });
+      }
